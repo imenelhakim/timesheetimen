e956974f66759c65c6010d494f835a6b812ab737
Imen EL HAKIM <imen.elhakim@esprit.tn>
2021-10-19 16:49:17 +0200

    timesheet folder

---
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -0,0 +1,23 @@
+package tn.esprit.spring.services;
+
+import java.util.List;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+
+public interface IEntrepriseService {
+	
+	public int ajouterEntreprise(Entreprise entreprise);
+	public int ajouterDepartement(Departement dep);
+	void affecterDepartementAEntreprise(int depId, int entrepriseId);
+	
+
+	//IIIMEEEENN
+	List<String> getAllDepartementsNamesByEntreprise(int entrepriseId);
+	public void deleteEntrepriseById(int entrepriseId);
+	public void deleteDepartementById(int depId);
+	//IIIMEEEENN
+	
+	
+	public Entreprise getEntrepriseById(int entrepriseId);
+}
